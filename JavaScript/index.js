dd52931b6ffa81e99894d56d6b4e4536925d6c41
AsithandileNyjah <atyuluu@gmail.com>
2023-12-01 08:53:38 +0000

    Some changes

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -16,47 +16,40 @@ function toDo() {
             completed: false
         };
 
-        // Create list item
+
         let listItem = document.createElement('li');
 
-        // Create checkbox
+
         let checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
         checkbox.addEventListener('change', () => {
-            toDoItem.completed = checkbox.checked;
+            toDoItem.completed = checkbox;
             updateTaskStatus();
         });
-
-        // Display task text
         let taskText = document.createElement('span');
         taskText.textContent = `${toDoItem.text} (Created on: ${toDoItem.timestamp})`;
 
-        // Create delete button
         let deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Delete';
+        deleteBtn.textContent = 'X';
         deleteBtn.addEventListener('click', () => {
             listItem.remove();
         });
 
-        // Append elements to the list item
         listItem.appendChild(checkbox);
         listItem.appendChild(taskText);
         listItem.appendChild(deleteBtn);
 
-        // Append the list item to the list
         list.appendChild(listItem);
 
-        // Clear the input field
-        document.querySelector('[enteredList]').value = '';
+        
+        enteredList.value = '';
     }
 }
 
 function updateTaskStatus() {
-    // Handle the task completion logic here
 }
 
 function sortTasks() {
-    // Sort tasks alphabetically
     let items = Array.from(list.children);
     items.sort((a, b) => {
         let textA = a.textContent.toLowerCase();
@@ -64,20 +57,8 @@ function sortTasks() {
         return textA.localeCompare(textB);
     });
 
-    // Clear the current list
     list.innerHTML = '';
 
-    // Append sorted tasks to the list
+    
     items.forEach(item => list.appendChild(item));
-}
-
-
-
-let klear = document.querySelector('[clear]')
-
-klear.addEventListener('click', clear)
-
-function clear (){
-    document.querySelector('[enteredList]').value = '';
-    document.querySelector('[list]').value = '';
 }
